ef7a5379d8487fb88cdf22350769552822b96201
Squidjigg <squidjigg80@outlook.com>
2025-05-09 19:34:56 +1000

    feat: update bash script to check that provisioner has run on CentOS boxes

--- 1_getting_started/scripts/update_centos_repos.sh
+++ 1_getting_started/scripts/update_centos_repos.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# Update CentOS repositories to use vault.centos.org
-sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/CentOS-*.repo
-sed -i s/^#.*baseurl=http/baseurl=http/g /etc/yum.repos.d/CentOS-*.repo
-sed -i s/^mirrorlist=http/#mirrorlist=http/g /etc/yum.repos.d/CentOS-*.repo
+if [ ! -f /root/provisioned ]; then
+    # Update CentOS repositories to use vault.centos.org
+    echo "INFO: Updating CentOS repositories."
+    sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/CentOS-*.repo
+    sed -i s/^#.*baseurl=http/baseurl=http/g /etc/yum.repos.d/CentOS-*.repo
+    sed -i s/^mirrorlist=http/#mirrorlist=http/g /etc/yum.repos.d/CentOS-*.repo
+    touch /root/provisioned
+fi
